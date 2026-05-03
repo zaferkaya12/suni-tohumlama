df298495b22310ae9fa9ac0e2bd710a58f150611
zaferkaya12 <zaferkaya12@gmail.com>
2026-05-03 15:53:58 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'suni-tohumlama-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+// Kurulum
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Önbellekten sun
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Eski önbellekleri temizle
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
