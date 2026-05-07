4b79e54d4267c571c7359ab6b592ec9c6159f12e
zaferkaya12 <zaferkaya12@gmail.com>
2026-05-07 09:52:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,60 @@
-const CACHE_NAME = 'suni-tohumlama-v1';
+const CACHE_NAME = 'suni-tohumlama-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
+  '/suni-tohumlama/',
+  '/suni-tohumlama/index.html',
+  '/suni-tohumlama/manifest.json',
+  '/suni-tohumlama/icon-192.png',
+  '/suni-tohumlama/icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
 ];
 
+// Install - Cache dosyaları
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache açıldı');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
+// Activate - Eski cache'leri temizle
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Eski cache siliniyor:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch - Önce cache, sonra network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // Cache edilebilir mi kontrol et
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
   );
 });
